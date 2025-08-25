659ffe10f42d48cad85f165aa5381723b5126ad4
Matheus Kur <matheuskur7@gmail.com>
2025-08-25 10:12:52 -0300

    Initial Commit

---
+++ CarRearIdentifier/CarRearIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRearIdentifierApp.swift
+//  CarRearIdentifier
+//
+//  Created by aluno-06 on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CarRearIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarRearIdentifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarRearIdentifier
+//
+//  Created by aluno-06 on 25/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
